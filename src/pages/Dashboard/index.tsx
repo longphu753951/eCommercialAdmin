30293e9d0aa6a37a37a1f4d2e1e712e079500a10
Long Phu <phutl@icd-vn.com>
2022-08-09 14:31:08 +0700

    test

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -7,6 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -18,8 +19,76 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Chart } from 'react-google-charts';
+import { Grid } from '@mui/material';
 
 const drawerWidth = 240;
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export const data = [
+  ['Year', 'Sales', 'Expenses'],
+  ['Jan', 1000, 400],
+  ['Feb', 1170, 460],
+  ['Mar', 660, 1120],
+  ['Apr', 1030, 540],
+  ['May', 1030, 540],
+  ['June', 1030, 540],
+  ['Jul', 1030, 540],
+  ['Aug', 1030, 540],
+  ['Sep', 1030, 540],
+  ['Oct', 1030, 540],
+  ['Nov', 1030, 540],
+  ['Dec', 1030, 540],
+];
+
+export const options = {
+  legend: { position: 'none' },
+  chart: {
+    title: 'Sales statistics',
+  },
+  colors: ['#2C7BE5', '#D2DDEC'],
+  width: 500,
+};
+
+export const pieData = [
+  ['Website growth', 'Hours per Day'],
+  ['Social Media', 11],
+  ['Affiliate visitors', 2],
+  ['Purchased visitors', 2],
+  ['By advertisement', 2],
+];
+
+export const pieOptions = {
+  legend: { position: 'bottom', maxLines: 3 },
+  pieHole: 0.9,
+  title: 'My Daily Activities',
+  colors: ['#2C7BE5', '#CDD2FD', '#96C0FF', '#CCF0D1'],
+  width: 500,
+};
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
@@ -77,7 +146,6 @@ const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
   width: drawerWidth,
-  color: 'green',
   flexShrink: 0,
   whiteSpace: 'nowrap',
   boxSizing: 'border-box',
@@ -106,7 +174,7 @@ export default function MiniDrawer() {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open}>
+      <AppBar position="fixed" open={open} color={'secondary'}>
         <Toolbar>
           <IconButton
             color="default"
@@ -120,7 +188,7 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
-          <input />
+          <InputBase />
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
@@ -186,14 +254,87 @@ export default function MiniDrawer() {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <Typography
-          sx={{ fontWeight: '600', color: '#474747' }}
-          variant="h5"
-          color={''}
-          component="div"
+        <Box
+          sx={{
+            width: 500,
+            height: 100,
+          }}
         >
-          Dashboard
-        </Typography>
+          <Typography
+            sx={{ fontWeight: '600', color: '#474747' }}
+            variant="h5"
+            color={''}
+            component="div"
+          >
+            Dashboard
+          </Typography>
+        </Box>
+        <Grid container spacing={8} sx={{ flexGrow: 1, px: 8 }}>
+          <Box
+            sx={{
+              width: 600,
+              height: 300,
+            }}
+          >
+            <Chart
+              chartType="Bar"
+              width="100%"
+              height="400px"
+              data={data}
+              options={options}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+            }}
+          >
+            <Chart
+              chartType="PieChart"
+              data={pieData}
+              options={pieOptions}
+              width={'100%'}
+              height={'400px'}
+            />
+          </Box>
+        </Grid>
+        <Box
+          sx={{
+            width: 800,
+            height: 300,
+          }}
+        >
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Dessert (100g serving)</TableCell>
+                  <TableCell align="right">Calories</TableCell>
+                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
     </Box>
   );
